834773eca0e359f1653f91fe03c4c914c6122f91
Rayan Al Othaim <r.alothaim@jodayn.com>
2023-08-07 16:04:18 +0300

    progress

--- api/src/test/java/API/New.java
+++ api/src/test/java/API/New.java
@@ -11,6 +11,7 @@ import org.testng.annotations.BeforeTest;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
@@ -27,7 +28,7 @@ public class New {
   }
 
   @Test
-  public void test() throws  IOException, ParseException, org.json.simple.parser.ParseException {
+  public void add() throws  IOException, ParseException, org.json.simple.parser.ParseException {
     String file = "C:\\Users\\RayanAlOthaim\\OneDrive - JODAYN\\Documents\\VSC\\API testing\\Proj0\\api\\src\\test\\java\\Payload\\payload.json";
     JSONParser parser = new JSONParser();      
         FileReader fileReader = new FileReader(file);
@@ -37,5 +38,19 @@ String f = json.toString();
    System.out.println(response);
     JsonPath js = new JsonPath(response);
     System.out.println(js.getString("place_id"));
+  }
+  @Test
+  public void update() throws IOException, org.json.simple.parser.ParseException{
+    String file = "C:\\Users\\RayanAlOthaim\\OneDrive - JODAYN\\Documents\\VSC\\API testing\\Proj0\\api\\src\\test\\java\\Payload\\payload2.json";
+    JSONParser parser = new JSONParser();      
+        FileReader fileReader = new FileReader(file);
+                JSONObject json = (JSONObject) parser.parse(fileReader);
+String f = json.toString();
+given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json").body(f).when().put("/maps/api/place/update/json").then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+  }
+  @Test
+  public void get(){
+    String gett = given().queryParam("key", "qaclick123").queryParam("place_id", "4bf0f7e4a1095cec0a978fddb8e13396").when().get("/maps/api/place/get/json").then().assertThat().log().all().statusCode(200).body("address", equalTo("70 Summer walk, USA")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+    System.out.println(gett);
   }
 }
